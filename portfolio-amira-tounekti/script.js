94cda315a14e67ee6ec500aa99385e52897d102b
terresteur <112128653+terresteur@users.noreply.github.com>
2025-10-08 13:37:20 -0400

    gsap-deplacement-section2-changement-image-dans-css

--- portfolio-amira-tounekti/script.js
+++ portfolio-amira-tounekti/script.js
@@ -1,3 +1,15 @@
 gsap.registerPlugin(ScrollTrigger);
 
+gsap.to(".deuxiemeSection", {
+    scrollTrigger: {
+      trigger: ".decoPresentation",
+      start: "top top",
+      end: "bottom top",
+      scrub: true,
+      pin: true,
+    },
+    y: "-100vh", 
+  });
+  
+
   
