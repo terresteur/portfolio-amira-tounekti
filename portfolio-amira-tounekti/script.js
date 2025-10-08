07f6f6b12d1a797a95bd8fc4556cbc9aea20a916
terresteur <112128653+terresteur@users.noreply.github.com>
2025-10-08 17:04:28 -0400

    test-gsap-fonctionne-pas

--- portfolio-amira-tounekti/script.js
+++ portfolio-amira-tounekti/script.js
@@ -1,15 +1,33 @@
 gsap.registerPlugin(ScrollTrigger);
 
 gsap.to(".deuxiemeSection", {
+  pin: false,
     scrollTrigger: {
       trigger: ".decoPresentation",
       start: "top top",
       end: "bottom top",
       scrub: true,
       pin: true,
+      markers: true,   
     },
     y: "-100vh", 
+    markers: true,
+    pin: true,  
   });
-  
+
+ /* gsap.to(".troisiemeSection", {
+    scrollTrigger: {
+      trigger: ".deuxiemeSection",
+      start: "center top",
+      end: "bottom top",
+      scrub: true,
+      pin: true,
+      y: "-200vh",
+      markers: true,   
+    },
+    y: "-100vh", 
+    markers: true,  
+  });
+  */
 
   
