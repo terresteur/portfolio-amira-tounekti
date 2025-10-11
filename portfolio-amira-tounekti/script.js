30ba82acc8fc40a7260c4331d272020629670d72
terresteur <112128653+terresteur@users.noreply.github.com>
2025-10-11 18:17:21 -0400

    animation-gsap-scroll-section-et-flocons-tombe-fonctionne-de-la-navigation

--- portfolio-amira-tounekti/script.js
+++ portfolio-amira-tounekti/script.js
@@ -1,33 +1,70 @@
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.to(".deuxiemeSection", {
-  pin: false,
-    scrollTrigger: {
-      trigger: ".decoPresentation",
-      start: "top top",
-      end: "bottom top",
-      scrub: true,
-      pin: true,
-      markers: true,   
-    },
-    y: "-100vh", 
-    markers: true,
-    pin: true,  
+ScrollTrigger.create({
+  trigger: ".decoPresentation",
+  start: "top top",
+  end: "bottom+=100% top",
+  pin: true,
+  pinSpacing: false,
+  markers: true,
+});
+
+ScrollTrigger.create({
+  trigger: ".deuxiemeSection",
+  start: "center center",
+  end: "bottom+=50% top",
+  pin: true,
+  pinSpacing: false,
+  markers: true,
+  onEnter: () => {
+    gsap.to(".presentation", { 
+      opacity: 1, 
+      duration: 1, 
+      ease: "power2.out",
+      scale:1,
+    });
+  },
+  onLeaveBack: () => {
+    gsap.to(".presentation", { 
+      opacity: 0, 
+      duration: 1, 
+      ease: "power2.out",
+      scale: 0.95,
+    });
+  }
+});
+
+  let boutonProjet = document.querySelector(".bouton.projet");
+  let boutonAccueil = document.querySelector(".bouton.accueil");
+
+  boutonProjet.addEventListener("click", () => {
+    window.location.href = "projets.html";
   });
 
- /* gsap.to(".troisiemeSection", {
-    scrollTrigger: {
-      trigger: ".deuxiemeSection",
-      start: "center top",
-      end: "bottom top",
-      scrub: true,
-      pin: true,
-      y: "-200vh",
-      markers: true,   
-    },
-    y: "-100vh", 
-    markers: true,  
+  boutonAccueil.addEventListener("click", () => {
+    window.location.href = "accueil.html";
   });
-  */
 
-  
+function animeFlocon(selector, delay = 0) {
+  const flocon = document.querySelector(selector);
+  const windowWidth = window.innerWidth;
+
+  gsap.set(flocon, { x: Math.random() * windowWidth, y: 0 });
+
+  gsap.to(flocon, {
+      y: window.innerHeight + 20, 
+      x: `+=${Math.random() * 100 - 0}`, 
+      duration: Math.random() * 5 + 5,
+      repeat: -1, 
+      ease: "linear",
+      delay: delay
+  });
+}
+
+// Lancer l'animation pour chaque flocon
+animeFlocon(".neige1", 0);
+animeFlocon(".neige2", 0.5);
+animeFlocon(".neige3", 1);
+animeFlocon(".neige4", 1.5);
+animeFlocon(".neige5", 2);
+animeFlocon(".neige6", 2.5);
