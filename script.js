00c631182bb7c74d8797e7ca15ec614095281593
terresteur <112128653+terresteur@users.noreply.github.com>
2025-11-09 21:50:06 -0500

    changement-pour-la-version-tablette-journee-carriere

--- script.js
+++ script.js
@@ -447,9 +447,9 @@ const swiper = new Swiper(".swiper", {
 
 const swiperResto = new Swiper(".swiperResto", {
   direction: "horizontal",
-  slidesPerView: 1,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
+    navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
+  slidesPerView: 1,
 });
